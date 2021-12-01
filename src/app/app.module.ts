2bf136743822cf21f5bcfaec7ea44b0d43605e82
Aldair Soares <aldair.soares.as@gmail.com>
2021-12-01 20:17:09 -0300

    Created Menu, footer, login and Subscribe pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    RodapeComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
