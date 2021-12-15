684120db1a4bd557afc00898bc5ef2220f8d2b80
Aldair Soares <aldair.soares.as@gmail.com>
2021-12-14 23:31:01 -0300

    enviando os arquivos finais do crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HashLocationStrategy,LocationStrategy} from '@angular/common';
+
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
+
 
 @NgModule({
   declarations: [
@@ -14,13 +28,25 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     LoginComponent,
     MenuComponent,
     RodapeComponent,
-    CadastroComponent
+    CadastroComponent,
+    InicioComponent,
+    TemaComponent,
+    TemaEditComponent,
+    TemaDeleteComponent,
+    PostagemEditComponent,
+    PostagemDeleteComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
